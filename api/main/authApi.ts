3f659c80d64fc28e2afa27778c123f2e24fe3ba1
overbye <overbye87@gmail.com>
2022-06-16 23:11:39 +0300

    fix type

--- api/main/authApi.ts
+++ api/main/authApi.ts
@@ -1,21 +1,19 @@
 import { ISignIn, ISignUp, IUser } from '../../types/main';
 import { authAxios } from './axios';
 
-type IResp = { token: string, data: IUser }
-
-type Response = {
-  data: IUser;
+interface IRes {
   token: string;
+  data: IUser;
 }
 
-export const check = async (): Promise<Response> => {
-  return authAxios.get<unknown, IResp>('/auth');
+export const check = async (): Promise<IRes> => {
+  return authAxios.get<unknown, IRes>('/auth');
 };
 
-export const signIn = async (SignInData: ISignIn): Promise<Response> => {
-  return authAxios.post<unknown, IResp>('/auth', SignInData);
+export const signIn = async (SignInData: ISignIn): Promise<IRes> => {
+  return authAxios.post<unknown, IRes>('/auth', SignInData);
 };
 
-export const signUp = async (SignUpData: ISignUp): Promise<Response> => {
-  return authAxios.post<unknown, IResp>('/auth/signup', SignUpData);
+export const signUp = async (SignUpData: ISignUp): Promise<IRes> => {
+  return authAxios.post<unknown, IRes>('/auth/signup', SignUpData);
 };
